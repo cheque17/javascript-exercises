0122a32fd23d6e9a375b77c61eaa513e4c519494
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-14 15:16:06 -0600

    05_sumAll/sumAll.js and 05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
+    let start = firstNum;
+    let end = secondNum;
 
+    if (firstNum > secondNum) {
+        start = secondNum;
+        end = firstNum;
+    }
+
+    if ( firstNum<0 || secondNum<0 || typeof(firstNum)!='number' || typeof(secondNum)!='number') {
+        return 'ERROR';
+    }
+
+    for (let i= start; i<=end; i++){
+        result += i;
+    }
+    return result;
+  /*  console.log(typeof(firstNum));
+    console.log(typeof(secondNum));*/
 };
 
 // Do not edit below this line
