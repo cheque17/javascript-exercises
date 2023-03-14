828595079b709071e69ad3d7a9ebe90f83aabdac
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-14 14:43:20 -0600

    Edit 04_removeFromArray/removeFromArray.js and 04_removeFromArray/removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+  let result = [];
+  let arrayLength = array.length;
 
-};
+  for (let i = 0; i < arrayLength; i++) {
+    let shouldRemove = false;
+    for (let elementToTake of elements) {
+      if (array[i] === elementToTake) {
+        shouldRemove = true;
+        break;
+      }
+    }
+    if (!shouldRemove) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
