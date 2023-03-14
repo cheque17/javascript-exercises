66eedbaa234deba5a09266b2d09a1ed0a394d930
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-13 18:12:08 -0600

    Edit 03_reverseString/reverseString.js, 03_reverseString/reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result='';
+    for (i= 1; i <= string.length; i++){
+        if (i===1){
+            result += string.slice(-(i));
+        } else {
+            result += string.slice(-(i), -(i-1));
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
