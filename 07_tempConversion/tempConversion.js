c2cf628453f8d20dc94dc1b5b52d284fdcfb999f
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-14 17:29:20 -0600

    Edit 07_tempConversion/tempConversion.js and 07_tempConversion/tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  let rawConversion = (fahrenheitTemperature-32)*5/9;
+  let roundedResult = rawConversion.toFixed(1);
+  let celsiusResult = parseFloat(roundedResult);
+  return celsiusResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let fahrenheitResult =  ( Math.round ( ( celsiusTemperature*9/5 + 32 ) * 10 ) /10 );
+  return fahrenheitResult;
 };
 
 // Do not edit below this line
