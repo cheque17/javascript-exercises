3de9039ed7fefbec8a4e63e2bd3a76e7e2543b85
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-30 20:20:44 -0600

    10_fibonacci/fibonacci.js and 10_fibonacci/fibonacci.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let forwardString= string.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let reverseString = forwardString.split("").reverse().join("");
+    if (forwardString === reverseString){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let values = [1,1];
+    if (position === 1 || position === 2){
+        return values[position-1];
+    } else if (position < 0) {
+        return 'OOPS';
+    }
+    let amountOfLoops= position - 2;
+    console.log(values[values.length-1]);
+    console.log(values[values.length-2]);
+    for (let i=0; i<amountOfLoops; i++){
+        values.push(values[values.length-1] + values[values.length-2])
+    };
+    console.log(values[values.length-1]);
+    console.log(values[values.length-2]);
+     let positionValue = values[values.length-1];
+    
+    return positionValue;
 };
 
 // Do not edit below this line
