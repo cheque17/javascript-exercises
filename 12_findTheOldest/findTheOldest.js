67e105781032e215d98b2622ea48878f30c31f4f
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-31 11:54:28 -0600

    11_getTheTitles/getTheTitles.js, 12_findTheOldest/findTheOldest.js, 12_findTheOldest/findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce( (firstPerson, secondPerson) => {
+        let firstPersonAge= calculateAge (firstPerson.yearOfBirth, firstPerson.yearOfDeath);
+        let secondPersonAge = calculateAge( secondPerson.yearOfBirth, secondPerson.yearOfDeath);
+        return firstPersonAge < secondPersonAge ? secondPerson :firstPerson;
+    } )
+}; 
 
-};
+const calculateAge = function (birthYear, lastYearReportedAlive) {
+    if (!lastYearReportedAlive) {
+      lastYearReportedAlive = new Date().getFullYear();
+    }
+    return lastYearReportedAlive - birthYear;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
